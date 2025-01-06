f74733079cb2e41a26b88b432334e39c759ef3e4
HANTAEDONG <htd0913@gmail.com>
2025-01-06 15:40:21 +0900

    [Style] login page 디자인 구현

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,5 +1,18 @@
+import LoginButton from '@/components/atoms/LoginButton';
+import DefaultLogo from '@/assets/logos/DefaultLogo.svg';
+
 const LoginPage = () => {
-  return <button>LoginPage</button>;
+  return (
+    <div className='flex justify-center w-full min-h-svh'>
+      <div className='w-[700px] min-h-full flex flex-col items-center gap-[20%] pt-[10%]'>
+        <img src={DefaultLogo} alt='로고' className='w-[410px] h-[165px]' />
+        <div className='flex flex-col gap-[20px]'>
+          <LoginButton iconType='github' label='Github로 시작하기' />
+          <LoginButton iconType='google' label='Google 시작하기' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
