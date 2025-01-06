83ddb35838925578e06a9af52f104dfc1352a3bb
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-01-06 22:18:43 +0900

    [Feat] IntroductionTemplate 수정

--- src/components/templates/MyPage/IntroductionTemplate.tsx
+++ src/components/templates/MyPage/IntroductionTemplate.tsx
@@ -1,7 +1,18 @@
 import Button from '@/components/atoms/Button';
 import ContributionBox from '@/components/molecules/ContributionBox';
+import MyPageProjectCard from '@/components/molecules/MyPageProjectCard';
+import { useTabsStore } from '@/store/tabStore';
+import { useShallow } from 'zustand/shallow';
+import GitHubCalendar from 'react-github-calendar';
+import { Link } from 'react-router-dom';
 
 const IntroductionTemplate = () => {
+  const [setActiveTab] = useTabsStore(
+    useShallow((state) => [state.setActiveTab])
+  );
+
+  const githubId = 'chaeyun-sim';
+
   return (
     <>
       <div className='h-[250px] py-[10px] flex items-center gap-[17px]'>
@@ -19,15 +30,47 @@ const IntroductionTemplate = () => {
             PAD Contribution
           </span>
           <div className='grid grid-cols-2 gap-[10px] mt-[15px]'>
-            <ContributionBox text='👥 팔로워' amount={199} />
-            <ContributionBox text='👥 팔로잉' amount={11} />
-            <ContributionBox text='💬 피드 작성 수' amount={199} />
-            <ContributionBox text='💡 지원 수' amount={2} />
+            <ContributionBox text='👥 팔로워' amount={199} onClick={() => {}} />
+            <ContributionBox text='👥 팔로잉' amount={11} onClick={() => {}} />
+            <ContributionBox
+              text='💬 피드 작성 수'
+              amount={199}
+              onClick={() => setActiveTab('피드')}
+            />
+            <ContributionBox
+              text='💡 지원 수'
+              amount={2}
+              onClick={() => setActiveTab('커넥션 허브')}
+            />
           </div>
         </div>
       </div>
-      {/* <div className='h-[166px]'></div>
-      <div className='h-[166px]'></div> */}
+
+      {/* 작업물 목록 */}
+      {githubId ? (
+        <Link
+          to={`https://github.com/${githubId}`}
+          className='flex justify-center bg-white border border-[#e1e1e1] rounded-[5px] pb-[10px] pt-4'
+        >
+          <GitHubCalendar
+            username={githubId}
+            blockSize={9.4}
+            fontSize={11}
+            showWeekdayLabels
+            blockMargin={3.2}
+          />
+        </Link>
+      ) : (
+        <div className='flex justify-center bg-white border border-[#e1e1e1] rounded-[5px] pb-[10px] pt-4 h-[158px]'>
+          hi
+        </div>
+      )}
+      <div className='grid grid-cols-2 gap-5'>
+        <MyPageProjectCard />
+        <MyPageProjectCard />
+        <MyPageProjectCard />
+        <MyPageProjectCard />
+      </div>
       <div className='flex items-center justify-center h-9'>
         <Button
           width='235px'
