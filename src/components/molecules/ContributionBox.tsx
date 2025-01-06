5241067d9aabb601f58d0c3d2e1fa4421752acfa
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-01-06 22:18:02 +0900

    [Fix] 컨트리뷰션 박스 수정 및 버튼화

--- src/components/molecules/ContributionBox.tsx
+++ src/components/molecules/ContributionBox.tsx
@@ -1,16 +1,20 @@
 interface ContributionBoxProps {
   text: string;
   amount: number;
+  onClick: () => void;
 }
 
-const ContributionBox = ({ text, amount }: ContributionBoxProps) => {
+const ContributionBox = ({ text, amount, onClick }: ContributionBoxProps) => {
   return (
-    <div className='w-[220px] h-[80px] rounded-[5px] bg-white flex flex-col justify-between p-[10px]'>
+    <button
+      className='w-[220px] h-[80px] rounded-[5px] bg-white flex flex-col justify-between p-[10px]'
+      onClick={onClick}
+    >
       <span className='text-darkgray font-medium text-[15px]'>{text}</span>
       <span className='text-[30px] font-semibold text-black text-right'>
         {amount}
       </span>
-    </div>
+    </button>
   );
 };
 
