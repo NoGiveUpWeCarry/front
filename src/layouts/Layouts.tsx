1811c11aac5a9f8e6a5df1f06f1c3a1e2e986df3
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-01-07 23:47:04 +0900

    [Feat] 레이아웃 정보 선택으로 수정, 라우트에 계정 설정 페이지 추가

--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import SideBar from '@/components/organisms/SideBar';
+import { Outlet, useLocation } from 'react-router-dom';
 
-const Layouts = ({ children }: { children: React.ReactNode }) => {
+const Layouts = () => {
+  const location = useLocation();
+  const preventInfo = ['/settings'];
+
   return (
     <div className='min-h-screen bg-[#F5F5F5] flex'>
       {/* Sidebar */}
@@ -10,22 +14,26 @@ const Layouts = ({ children }: { children: React.ReactNode }) => {
       {/* Main Content */}
       <div className='flex-1 p-6'>
         {/* Header */}
-        <div className='flex justify-end'>
-          <div className='w-[300px] bg-white shadow-md p-4'>
-            <h2 className='font-bold'>PAD Contact</h2>
-            <p className='text-sm'>
-              PAD Inc.
-              <br />
-              Republic of Korea
-              <br />
-              123-456-7890
-              <br />
-              info@pad.com
-            </p>
+        {!preventInfo.includes(location.pathname) && (
+          <div className='absolute flex justify-end right-0 mr-6'>
+            <div className='w-[300px] bg-white shadow-md p-4'>
+              <h2 className='font-bold'>PAD Contact</h2>
+              <p className='text-sm'>
+                PAD Inc.
+                <br />
+                Republic of Korea
+                <br />
+                123-456-7890
+                <br />
+                info@pad.com
+              </p>
+            </div>
           </div>
-        </div>
+        )}
         {/* Page Content */}
-        <div className='mt-6'>{children}</div>
+        <div className='mt-6'>
+          <Outlet />
+        </div>
       </div>
     </div>
   );
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,29 +2,30 @@ import ChatPage from '@/components/pages/ChatPage';
 import LoginPage from '@/components/pages/LoginPage';
 import HomePage from '@/components/pages/HomePage';
 import Layouts from '@/layouts/Layouts';
+import SettingsPage from '@/components/pages/SettingsPage';
 
 const router = [
   {
     path: '/',
-    element: (
-      <Layouts>
-        <HomePage />
-      </Layouts>
-    ),
+    element: <Layouts />,
     children: [
       {
         path: '/',
         element: <HomePage />,
       },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/chat',
+        element: <ChatPage />,
+      },
+      {
+        path: '/settings',
+        element: <SettingsPage />,
+      },
     ],
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/chat',
-    element: <ChatPage />,
   },
 ];
 
