1eed2e7818559589ea3e0d6609eb7969b506bfeb
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-01-07 18:07:48 +0900

    [Feat] 소개 탭 페이지에 모달 적용, 프로젝트 목록 수정

--- src/components/templates/MyPage/IntroductionTemplate.tsx
+++ src/components/templates/MyPage/IntroductionTemplate.tsx
@@ -4,18 +4,52 @@ import { useTabsStore } from '@/store/tabStore';
 import { useShallow } from 'zustand/shallow';
 import WorkList from '@/components/organisms/WorkList';
 import { useState } from 'react';
+import AddProjectModal from '@/components/organisms/modals/AddProjectModal';
+import FollowersModal from '@/components/organisms/modals/FollowersModal';
+import { useFollowsStore } from '@/store/followsStore';
+import AddMusicModal from '@/components/organisms/modals/AddMusicModal';
 
 const IntroductionTemplate = () => {
   const [setActiveTab] = useTabsStore(
     useShallow((state) => [state.setActiveTab])
   );
+  const [setFollows] = useFollowsStore(
+    useShallow((state) => [state.setFollows])
+  );
 
-  const [role] = useState<'Programmer' | 'Designer' | 'Artist'>('Designer');
+  const [role] = useState<'Programmer' | 'Designer' | 'Artist'>('Artist');
   const [countWorks, setCountWorks] = useState(0);
-  // 디자이너 - 프로젝트 4, 아티스트 - 음악 3, 개발자 - 프로젝트 2 + 깃허브
+  const [isAddProjectOpen, setIsAddProjectOpen] = useState(false);
+  const [isFollowersOpen, setIsFollowersOpen] = useState(false);
+
+  const PROJECT_LIMIT = {
+    Designer: 4,
+    Artist: 3,
+    Programmer: 2,
+  };
+
+  const handleFollow = (type: 'wer' | 'ing') => {
+    if (type === 'wer') {
+      // 로직
+      // setFollows()
+    } else {
+      // 로직
+      // setFollows()
+    }
+    setIsFollowersOpen(true);
+  };
 
   return (
     <>
+      {isAddProjectOpen &&
+        (role === 'Artist' ? (
+          <AddMusicModal onClose={() => setIsAddProjectOpen(false)} />
+        ) : (
+          <AddProjectModal onClose={() => setIsAddProjectOpen(false)} />
+        ))}
+      {isFollowersOpen && (
+        <FollowersModal onClose={() => setIsFollowersOpen(false)} />
+      )}
       <div className='h-[250px] py-[10px] flex items-center gap-[17px]'>
         <div className='flex flex-col gap-[10px] bg-status w-[230px] h-[230px] rounded-[20px] py-4 px-4 relative'>
           <span className='text-[15px] font-semibold text-white'>
@@ -31,8 +65,16 @@ const IntroductionTemplate = () => {
             PAD Contribution
           </span>
           <div className='grid grid-cols-2 gap-[10px] mt-[15px]'>
-            <ContributionBox text='👥 팔로워' amount={199} onClick={() => {}} />
-            <ContributionBox text='👥 팔로잉' amount={11} onClick={() => {}} />
+            <ContributionBox
+              text='👥 팔로워'
+              amount={199}
+              onClick={() => handleFollow('wer')}
+            />
+            <ContributionBox
+              text='👥 팔로잉'
+              amount={11}
+              onClick={() => handleFollow('ing')}
+            />
             <ContributionBox
               text='💬 피드 작성 수'
               amount={199}
@@ -46,36 +88,80 @@ const IntroductionTemplate = () => {
           </div>
         </div>
       </div>
+
+      {/* 프로젝트 예시 시작 */}
       {role == 'Artist' && (
         <WorkList>
           <WorkList.SoundCloud url='https://soundcloud.com/rudeadyet/ony-if-you-stayed?in=sc-playlists-kr/sets/dreamy-folk&si=fe25f7c999a844678e9ce1a0121b6061&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing' />
           <WorkList.Spotify url='https://open.spotify.com/playlist/37i9dQZF1E4A4Wx1igYfpM?si=STijBj6ET82XA_jOJKnUzQ' />
-          <WorkList.Spotify url='https://open.spotify.com/artist/6YVMFz59CuY7ngCxTxjpxE?si=_sK-4EzWS8WDKMbLJwKjvQ' />
+          {/* <WorkList.Spotify url='https://open.spotify.com/artist/6YVMFz59CuY7ngCxTxjpxE?si=_sK-4EzWS8WDKMbLJwKjvQ' /> */}
         </WorkList>
       )}
       {role == 'Programmer' && (
         <WorkList>
-          <WorkList.Github githubId={'chaeyun-sim'} />
-          <WorkList.Projects projectData={[{}, {}]} />
+          <WorkList.Github />
+          <WorkList.Projects>
+            <WorkList.ProjectItem
+              url=''
+              platforms={[]}
+              title=''
+              description=''
+            />
+            <WorkList.ProjectItem
+              url=''
+              platforms={[]}
+              title=''
+              description=''
+            />
+          </WorkList.Projects>
         </WorkList>
       )}
       {role == 'Designer' && (
         <WorkList>
-          <WorkList.Projects projectData={[{}, {}, {}, {}]} />
+          <WorkList.Projects>
+            <WorkList.ProjectItem
+              url=''
+              platforms={[]}
+              title=''
+              description=''
+            />
+            <WorkList.ProjectItem
+              url=''
+              platforms={[]}
+              title=''
+              description=''
+            />
+            <WorkList.ProjectItem
+              url=''
+              platforms={[]}
+              title=''
+              description=''
+            />
+            <WorkList.ProjectItem
+              url=''
+              platforms={[]}
+              title=''
+              description=''
+            />
+          </WorkList.Projects>
         </WorkList>
       )}
+      {/* 프로젝트 예시 끝 */}
 
-      <div className='flex items-center justify-center h-9'>
-        <Button
-          width='235px'
-          height='36px'
-          variants='filled'
-          radius='md'
-          className='!text-black border border-[#DCDCDC] bg-white'
-        >
-          + 작업물 추가하기
-        </Button>
-      </div>
+      {countWorks < PROJECT_LIMIT[role] && (
+        <div className='flex items-center justify-center h-9'>
+          <Button
+            width='235px'
+            height='36px'
+            variants='filled'
+            radius='md'
+            className='!text-black border border-[#DCDCDC] bg-white'
+            onClick={() => setIsAddProjectOpen(true)}
+          >
+            + 작업물 추가하기
+          </Button>
+        </div>
+      )}
     </>
   );
 };
