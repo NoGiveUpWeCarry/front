875f2076055b5e0310f250f0b282d0298273b475
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-01-08 15:59:46 +0900

    [Style] 스크롤바 디자인

--- src/components/organisms/chat/ChatMessages.tsx
+++ src/components/organisms/chat/ChatMessages.tsx
@@ -1,34 +1,43 @@
-import Avatar from '@/components/atoms/Avatar';
 import Date from '@/components/atoms/Date';
-import MessageBubble from '@/components/atoms/MessageBubble';
 import WelcomeMessage from '@/components/molecules/chat/WelcomeMessage';
+import Message from '@/components/organisms/chat/Message';
+// import useAuthStore from '@/store/authStore';
 import { Message as IMessage } from '@/types/chat.type';
+import clsx from 'clsx';
 
 interface ChatMessagesProps {
   messages: IMessage[];
 }
 
 const ChatMessages = ({ messages }: ChatMessagesProps) => {
+  // const userInfo = useAuthStore((state) => state.userInfo);
+  const userInfo = {
+    id: 'me',
+  };
+  if (!userInfo) return null;
+
   return (
-    <div className='grow px-[56px] flex flex-col gap-[20px]'>
+    <div
+      className={clsx(
+        'grow pl-[56px] pr-[44px] flex flex-col gap-[24px] scrollbar overflow-hidden hover:overflow-auto mr-[12px] hover:mr-0'
+      )}
+    >
       <WelcomeMessage />
       <Date className='text-gray text-caption2 text-center mt-[20px]'>
         2025년 1월 2일
       </Date>
-      {messages.map((message, i) => (
-        <>
-          {i > 0 && message.sender === messages[i - 1].sender ? (
-            <MessageBubble content={message.content} />
-          ) : (
-            <div>
-              <Avatar />
-              <div>{message.sender}</div>
-              <div>role</div>
-              <MessageBubble content={message.content} />
-            </div>
-          )}
-        </>
-      ))}
+      {messages.map((message, i) => {
+        return (
+          <Message
+            key={message.sender.id}
+            message={message}
+            sameBefore={
+              i > 1 && message.sender.id === messages[i - 1].sender.id
+            }
+            isMyMessage={message.sender.id === userInfo.id}
+          />
+        );
+      })}
     </div>
   );
 };
@@ -37,6 +46,6 @@ export default ChatMessages;
 
 /*
 1. 한 명이 연속으로 메세지를 여러개 보낸 경우 프로필 이미지는 하나만
-2. 날짜는 '일' 기준으로 하나씩만
+2. 날짜는 '일' 기준으로 하나씩만 (해당 채팅방에서 그 날 첫 메시지일경우 그 날의 날짜 표시)
 
 */
