569ba100f3fd38626c04866c898f25c48815f57e
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-01-08 12:41:26 +0900

    [Feat] 게정 설정 페이지에 설정 섹션 추가

--- src/components/pages/SettingsPage.tsx
+++ src/components/pages/SettingsPage.tsx
@@ -7,14 +7,14 @@ const SettingsPage = () => {
   return (
     <div className='w-full mt-[-24px] relative'>
       <div className='w-[250px] h-screen absolute left-0'></div>
-      <div className='w-[783px] h-screen mx-auto flex flex-col gap-[100px]'>
-        <div className='w-[700px] h-full mx-auto'>
+      <div className='w-[783px] min-h-screen mx-auto'>
+        <div className='w-[700px] h-full mx-auto flex flex-col gap-[100px]'>
           <SettingsSection>
             <SettingsSection.Title>기본 정보</SettingsSection.Title>
             <SettingsSection.Description>
               나를 소개해 보세요.
             </SettingsSection.Description>
-            <SettingsSection.Content>
+            <SettingsSection.Content gap={20}>
               <div className='mt-5 flex gap-[22px] py-[10px]'>
                 <Avatar size='md' />
                 <div className='flex flex-col'>
@@ -42,7 +42,7 @@ const SettingsPage = () => {
                   </div>
                 </div>
               </div>
-              <div className='mt-[20px] flex flex-col gap-[20px]'>
+              <div className='flex flex-col gap-[20px]'>
                 <SettingsSection.InputWithLabel label='한 줄 소개' />
                 <div className='flex justify-between'>
                   <SettingsSection.InputWithLabel
@@ -68,7 +68,60 @@ const SettingsPage = () => {
               </div>
             </SettingsSection.Content>
           </SettingsSection>
-          <div></div>
+          <SettingsSection>
+            <SettingsSection.Title>알림 설정</SettingsSection.Title>
+            <SettingsSection.Description>
+              필요한 알림만 받아보세요.
+            </SettingsSection.Description>
+            <SettingsSection.Content gap={20}>
+              <SettingsSection.TextWithToggle
+                title='푸시 알림'
+                description='웹 푸시 알림을 받아볼 수 있어요'
+                active={false}
+                toggle={() => {}}
+              />
+              <SettingsSection.TextWithToggle
+                title='피드 알림'
+                description='팔로잉하고 있는 유저의 피드가 업데이트되면 알림을 받아볼 수 있어요.'
+                active={true}
+                toggle={() => {}}
+              />
+              <SettingsSection.TextWithToggle
+                title='프로젝트 / 외주 알림'
+                description='북마크하고 있는 프로젝트 또는 외주 모집 마감 알림을 받아볼 수 있어요.'
+                active={true}
+                toggle={() => {}}
+              />
+            </SettingsSection.Content>
+          </SettingsSection>
+          <SettingsSection>
+            <SettingsSection.Title>계정 설정</SettingsSection.Title>
+            <SettingsSection.Description>
+              계정 정보를 확인하고 수정할 수 있어요.
+            </SettingsSection.Description>
+            <SettingsSection.Content gap={20}>
+              <div>
+                <div className='flex gap-[100px] items-center'>
+                  <label>연결된 이메일</label>
+                  <span>pad@gmail.com</span>
+                </div>
+                <span className='text-[10px] text-[#7D7D7D] mt-[-10px]'>
+                  이메일은 변경할 수 없어요.
+                </span>
+              </div>
+              <SettingsSection.InputWithLabel
+                label='유저 네임'
+                button={{
+                  text: '변경',
+                  color: 'normal',
+                }}
+              />
+              <span className='text-[10px] text-[#7D7D7D] mt-[-10px]'>
+                유저네임을 변경하면 프로필 페이지 링크도 변경됩니다.
+              </span>
+              <SettingsSection.Divider />
+            </SettingsSection.Content>
+          </SettingsSection>
         </div>
       </div>
     </div>
