4acfe550044722a49a3f62a513f273e5889834c7
HANTAEDONG <htd0913@gmail.com>
2025-01-11 16:54:57 +0900

    [Test] 테스트위해 홈페이지에 useEffect 추가

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 const HomePage = () => {
   useEffect(() => {
     const userId = useAuth.getState().userInfo?.id;
-    console.log('user_id: ' + userId);
+    console.log('홈페이지의 user_id: ' + userId);
   }, [useAuth.getState().userInfo?.id]);
   return (
     <div className='flex flex-col gap-[30px]'>
