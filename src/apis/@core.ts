10c6df4331363059d57946d1fffee0b6e03ec37b
HANTAEDONG <htd0913@gmail.com>
2025-01-11 16:31:52 +0900

    [Refactor] 토큰 업데이트 로직 수정

--- src/apis/@core.ts
+++ src/apis/@core.ts
@@ -1,6 +1,21 @@
-import axios from 'axios';
-import useAuth from '@/store/useAuth';
+import axios, { AxiosResponse } from 'axios';
 import { API_PATH } from '@/apis/api-path';
+import useAuth from '@/store/useAuth.store';
+import { User } from '@/types/user.type';
+
+interface RefreshRequest {
+  user_id: Pick<User, 'id'>;
+}
+
+interface Message {
+  code: number;
+  text: string;
+}
+
+interface RefreshResopnse {
+  message: Message;
+  access_token: string;
+}
 
 axios.defaults.baseURL = import.meta.env.BASE_SERVER_URL;
 
@@ -24,19 +39,24 @@ axiosInstance.interceptors.response.use(
   async (error) => {
     if (error.response?.status === 401) {
       try {
-        // Refresh Token으로 Access Token 갱신
         console.log('updateToken 패칭 요청됨.');
-        const refreshResponse = await axiosInstance.post(API_PATH.updateToken);
+        const user_id = useAuth.getState().userInfo?.id;
+        // 반환 타입 명시
+        const refreshResponse = await axiosInstance.post<
+          RefreshRequest,
+          AxiosResponse<RefreshResopnse>
+        >(API_PATH.updateToken, { user_id }); // POST 요청의 데이터는 객체로 전달해야 함.
         console.log('updateToken 패칭 실시됨.');
-        const { accessToken } = refreshResponse.data;
+        const { access_token } = refreshResponse.data;
         // 새 Access Token을 상태 저장소에 저장
-        useAuth.getState().setAccessToken(accessToken);
+        useAuth.getState().setAccessToken(access_token);
+        console.log('zustand에 업데이트');
         // 원래 요청을 다시 실행
-        error.config.headers.Authorization = `Bearer ${accessToken}`;
+        error.config.headers.Authorization = `Bearer ${access_token}`;
+        console.log('원래 요청 다시 실행됨.');
         return axiosInstance.request(error.config);
       } catch (refreshError) {
         console.error('Refresh Token 만료:', refreshError);
-        // 사용자를 로그인 페이지로 리디렉션
         window.location.href = '/login';
         return Promise.reject(refreshError);
       }
