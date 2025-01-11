89d31935aafe57659f4171bc75dab6989a91bb49
HANTAEDONG <htd0913@gmail.com>
2025-01-11 15:02:47 +0900

    [Feat] tiptapEditor에서 링크 구현 완료

--- src/hooks/tiptap/useEditor.hook.ts
+++ src/hooks/tiptap/useEditor.hook.ts
@@ -1,11 +1,16 @@
+import { useEditor } from '@tiptap/react';
+import { Editor } from '@tiptap/core';
 import TaskItem from '@tiptap/extension-task-item';
 import TaskList from '@tiptap/extension-task-list';
-import { useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Image from '@tiptap/extension-image';
 import Link from '@tiptap/extension-link';
 
-export const useCreateTiptapEditor = () =>
+type UseCreateTiptapEditor = (
+  setContent: React.Dispatch<React.SetStateAction<string>>
+) => Editor | null;
+
+export const useCreateTiptapEditor: UseCreateTiptapEditor = (setContent) =>
   useEditor({
     extensions: [
       StarterKit,
@@ -13,23 +18,18 @@ export const useCreateTiptapEditor = () =>
       TaskItem,
       Image,
       Link.configure({
-        openOnClick: false, // 기본 동작을 false로 설정
+        openOnClick: true,
         linkOnPaste: true,
       }),
     ],
     editorProps: {
       attributes: {
         class:
-          'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
-      },
-      handleClick(view, pos, event) {
-        const { href } = view.state.doc.nodeAt(pos)?.attrs || {};
-        if (href) {
-          window.open(href, '_blank'); // 새 창으로 열기
-          return true; // 이벤트 처리 완료
-        }
-        return false; // 다른 기본 클릭 동작 유지
+          'tiptap-editor prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
       },
     },
     content: '',
+    onUpdate: ({ editor }) => {
+      setContent(editor.getHTML()); // 에디터 내용 업데이트
+    },
   });
