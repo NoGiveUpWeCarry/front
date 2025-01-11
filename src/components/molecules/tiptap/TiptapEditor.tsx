f20244a3e6bd51e1c4554290a13f526ec4f10eba
HANTAEDONG <htd0913@gmail.com>
2025-01-11 15:02:16 +0900

    [Refactor] tiptapEditor에서 content를 state로 분리

--- src/components/molecules/tiptap/TiptapEditor.tsx
+++ src/components/molecules/tiptap/TiptapEditor.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import { EditorContent } from '@tiptap/react';
+import { Editor } from '@tiptap/core';
 import TiptapFloatingMenu from './TiptapFloatingMenu';
 import TiptapBubbleMenu from './TiptapBubbleMenu';
 import { useCreateTiptapEditor } from '@/hooks/tiptap/useEditor.hook';
 
 const TiptapEditor = () => {
-  const editor = useCreateTiptapEditor();
+  const [content, setContent] = useState<string>('');
+  const editor: Editor | null = useCreateTiptapEditor(setContent);
+
   return (
-    <div className='relative'>
-      <EditorContent editor={editor} className='border p-4' />
+    <div className='relative w-100%'>
+      <EditorContent editor={editor} className='p-4' />
       {editor && <TiptapFloatingMenu editor={editor} />}
       {editor && <TiptapBubbleMenu editor={editor} />}
     </div>
