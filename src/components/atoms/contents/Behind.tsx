3516ec09fa77dfd0e6ba44a1de2649b495dab432
leechan <dlcks0601@gmail.com>
2025-01-13 00:25:13 +0900

    [Feat] 뒤로가기 컴포넌트 구현

---
+++ src/components/atoms/contents/Behind.tsx
@@ -0,0 +1,11 @@
+import Icon from '@/components/atoms/Icon';
+
+const Behind = () => {
+  return (
+    <div className='flex w-full'>
+      <Icon type='behind' color={'black'} className='w-[24px] h-[24px]' />
+    </div>
+  );
+};
+
+export default Behind;
