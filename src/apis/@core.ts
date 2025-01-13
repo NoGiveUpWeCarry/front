2182efbdfc92ddb9aa519d9f88301e3476fde6bc
HANTAEDONG <htd0913@gmail.com>
2025-01-13 14:40:16 +0900

    [Refactor] 경로 수정

--- src/apis/@core.ts
+++ src/apis/@core.ts
@@ -38,20 +38,28 @@ axiosInstance.interceptors.request.use((config) => {
 
 axiosInstance.interceptors.response.use(
   (response) => {
+    const { message, ...rest } = response.data;
     return response;
   },
   async (error) => {
     if (error.response?.status === 401) {
       try {
         console.log('updateToken 패칭 요청됨.');
-        const user_id = useAuth.getState().userInfo?.id;
-        const refreshResponse = await axios.post(API_PATH.updateToken, user_id);
-        console.log('updateToken 패칭 실시됨.');
-        const { accessToken } = refreshResponse.data;
-        // 새 Access Token을 상태 저장소에 저장
-        useAuth.getState().setAccessToken(accessToken);
-        // 원래 요청을 다시 실행
-        error.config.headers.Authorization = `Bearer ${accessToken}`;
+        const user_id = getUserId();
+        if (!user_id) {
+          console.error('User ID가 존재하지 않습니다. 로그아웃 처리 중...');
+          window.location.href = '/login';
+          return Promise.reject('User ID가 없습니다.');
+        }
+        const refreshResponse = await axios.post<
+          RefreshRequest,
+          AxiosResponse<RefreshResopnse>
+        >(axios.defaults.baseURL + API_PATH.updateToken, { user_id });
+        const { access_token } = refreshResponse.data;
+        useAuth.getState().setAccessToken(access_token);
+        console.log('zustand에 업데이트');
+        error.config.headers.Authorization = `Bearer ${access_token}`;
+        console.log('원래 요청 다시 실행됨.');
         return axiosInstance.request(error.config);
       } catch (refreshError) {
         console.error('Refresh Token 만료:', refreshError);
