e0de5b79d4703e19d5dd01f2fc1fd50c925eb8d3
HANTAEDONG <htd0913@gmail.com>
2025-01-13 16:52:04 +0900

    [Feat] development환경에서 msw동작하도록 구현

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-
 async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
     return;
