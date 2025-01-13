ad108203a6833c6eb7afce8da0a7a8cb5cdf2d6f
HANTAEDONG <htd0913@gmail.com>
2025-01-13 16:46:30 +0900

    [Move] 기존 mock data들을 mocks 하위 mock-data폴더로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,22 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
+
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+  const { worker } = await import('@/mocks/browser.ts');
+
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  });
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
