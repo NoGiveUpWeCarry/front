e516ab57b8dd66bbb7f4b2d26895dc7902da2457
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-01-21 20:23:47 +0900

    [Feat] Feed 이미지 max height 고정

--- src/components/atoms/contents/ContentsThumbnail.tsx
+++ src/components/atoms/contents/ContentsThumbnail.tsx
@@ -3,6 +3,8 @@ interface ContentsThumbnailProps {
   alt?: string;
 }
 
+// 1.21 피드 이미지 max-height 고정 (마이페이지)
+
 const ContentsThumbnail = ({
   src,
   alt = 'Thumbnail',
@@ -12,7 +14,7 @@ const ContentsThumbnail = ({
       <img
         src={src}
         alt={alt}
-        className='w-full h-auto max-h-[177.5px] object-cover rounded-[20px]'
+        className='w-full h-auto max-h-[120px] object-cover rounded-[20px]'
       />
     </div>
   );
