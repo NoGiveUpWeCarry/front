9039595e7acf994099ee81b8d7b2abd02e7b625d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-01-27 19:55:12 +0900

    [Feat] 검색 페이지 컴포넌트 분리 및 무한 스크롤 적용

--- src/components/pages/SearchPage.tsx
+++ src/components/pages/SearchPage.tsx
@@ -1,27 +1,13 @@
-import { RoleProps } from '@/components/atoms/Role';
-import { HubContents } from '@/components/molecules/contents/ContentsItem';
-import { FeedContents } from '@/components/molecules/contents/FeedContentsItem';
-import { HubTagItemsKey } from '@/constants/hub/hubTagItems';
-import { meetingTagItemskey } from '@/constants/hub/meetingTagItems';
-import { roleTagItemsKey } from '@/constants/hub/roleTagsItems';
-import { statusTagItemskey } from '@/constants/hub/statusTagItems';
-import { TagItemKey } from '@/constants/tagItem';
-import {
-  useSearchConnectionHub,
-  useSearchFeed,
-} from '@/hooks/queries/search.query';
+import FeedView from '@/components/organisms/search/FeedView';
+import ProjectView from '@/components/organisms/search/ProjectView';
 import { useSearchTabsStore } from '@/store/searchTabsStore';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { useShallow } from 'zustand/shallow';
 
 const SearchPage = () => {
   const { search } = useLocation();
   const query = new URLSearchParams(search);
   const keyword = query.get('q') as string;
-
-  const { data: hubs, isLoading: isPostsLoading } =
-    useSearchConnectionHub(keyword);
-  const { data: feeds, isLoading: isProjectsLoading } = useSearchFeed(keyword);
 
   const { tabs, activeTab, setActiveTab } = useSearchTabsStore(
     useShallow((state) => state)
@@ -49,87 +35,9 @@ const SearchPage = () => {
         </div>
       </div>
       <div className='mt-5'>
-        {activeTab === '피드' &&
-          (isPostsLoading ? (
-            <div>검색 결과를 가져오는 중 입니다...</div>
-          ) : (
-            <div className='flex flex-col gap-10'>
-              {feeds?.pages[0].posts?.map((post) => (
-                <FeedContents
-                  key={post.postId}
-                  {...post}
-                  feedTags={post.tags as TagItemKey[]}
-                  thumnailUrl={post.thumbnailUrl}
-                  postId={Number(post.postId)}
-                  commentsCount={post.commentCount}
-                  likesCount={post.likeCount}
-                  viewsCount={post.viewCount}
-                  user={{
-                    avatarSrc: post.userProfileUrl,
-                    name: post.userNickname,
-                    job: post.userRole,
-                    time: post.createdAt,
-                  }}
-                />
-              ))}
-            </div>
-          ))}
-        {activeTab === '프로젝트' &&
-          (isProjectsLoading ? (
-            <div>검색 결과를 가져오는 중 입니다...</div>
-          ) : (
-            <div className='flex flex-col gap-10'>
-              {hubs?.pages[0].projects?.map((project) => (
-                <Link to={`/connectionhub/${project.projectId}`}>
-                  <HubContents
-                    key={project.projectId}
-                    {...project}
-                    user={{
-                      userProfileUrl: project.userProfileUrl,
-                      userNickname: project.userNickname,
-                      userRole: project.userRole,
-                      createdAt: project.createdAt,
-                    }}
-                    title={project.title}
-                    meetingTags={project.workType as meetingTagItemskey}
-                    statusTags={project.status as statusTagItemskey}
-                    hubTags={project.hubType as HubTagItemsKey}
-                    roleTags={
-                      (project.detailRoles ||
-                        project.skills) as roleTagItemsKey[]
-                    }
-                    role={project.role as RoleProps['role']}
-                    bookmarkCount={project.bookMarkCount}
-                    userCount={project.applyCount}
-                    viewsCount={project.viewCount}
-                    thumbnailUrl={project.userProfileUrl}
-                  />
-                </Link>
-              ))}
-            </div>
-          ))}
+        {activeTab === '피드' && <FeedView keyword={keyword} />}
+        {activeTab === '프로젝트' && <ProjectView keyword={keyword} />}
       </div>
-
-      {/* {active === '피드' &&
-       */}
-      {/* {active === '프로젝트' && (
-        <HubContents
-          key={item.title + new Date().toISOString()}
-          user={item.user}
-          title={item.title}
-          meetingTags={item.meetingTags}
-          statusTags={item.statusTags}
-          hubTags={item.hubTags}
-          roleTags={item.roleTags}
-          role={item.role}
-          bookmarkCount={item.bookmarkCount}
-          userCount={item.userCount}
-          viewsCount={item.viewsCount}
-          thumbnailUrl={item.thumbnailUrl}
-          startDate={item.startDate}
-          duration={item.duration}
-        />
-      )} */}
     </div>
   );
 };
