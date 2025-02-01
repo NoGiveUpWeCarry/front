2dd4b794b9058db7282ebbd086f2196cca9430f9
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-02-01 19:17:40 +0900

    [Feat] 유저 팝업 구현

--- src/components/molecules/contents/ContentsUser.tsx
+++ src/components/molecules/contents/ContentsUser.tsx
@@ -1,11 +1,15 @@
 import Avatar from '@/components/atoms/Avatar';
 import ContentsUserTitle from '@/components/atoms/contents/ConentsUserTitle';
-
+import Icon from '@/components/atoms/Icon';
+import Popup from '@/components/molecules/Popup';
+import { useModal } from '@/hooks/useModal';
+import { useNavigate } from 'react-router-dom';
 interface ContentsUserProps {
   userProfileUrl: string;
   createdAt: string;
   name: string;
   userRole: string;
+  userId: number;
 }
 
 const ContentsUser = ({
@@ -13,10 +17,39 @@ const ContentsUser = ({
   name,
   userRole,
   userProfileUrl,
+  userId,
 }: ContentsUserProps) => {
+  const navigate = useNavigate();
+  const { isOpen, openModal, closeModal } = useModal();
+  console.log(userId);
+
   return (
     <div className='flex items-start space-x-3'>
-      <Avatar src={userProfileUrl} size='xs' alt={`${name} Avatar`} />
+      <div
+        className='cursor-pointer relative'
+        onClick={() => (isOpen ? closeModal() : openModal())}
+      >
+        <Avatar src={userProfileUrl} size='xs' alt={`${name} Avatar`} />
+        {isOpen && (
+          <Popup
+            position='bottom'
+            popupHandler={[
+              {
+                onClick: () => navigate(`/@${name}`),
+                text: '마이페이지',
+                icon: <Icon type='user' className='w-4' />,
+              },
+              {
+                onClick: () =>
+                  navigate('/chat', { state: { targetUserId: userId } }),
+                text: '메세지 보내기',
+                icon: <Icon type='mail' className='w-4' />,
+              },
+            ]}
+            className='text-[13px]'
+          />
+        )}
+      </div>
       <ContentsUserTitle
         userNickname={name}
         userRole={userRole}
