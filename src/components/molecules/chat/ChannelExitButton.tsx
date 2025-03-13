17a70ae8d7439bc8a328a6d697c1b629160007b3
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-03-14 00:12:43 +0900

    [Fix] 이벤트 전파 차단
    - 이벤트 전파를 막지 않으면 이벤트 버블링 단계에서 ChannelList 의 리스트 아이템에 클릭 이벤트가 전파되어 채팅방이 나가지는게 아니라 채팅방에 들어가짐.

--- src/components/molecules/chat/ChannelExitButton.tsx
+++ src/components/molecules/chat/ChannelExitButton.tsx
@@ -3,6 +3,7 @@ import useAuthStore from '@/store/authStore';
 import { useChatStore } from '@/store/chatStore';
 import { Channel } from '@/types/channel.type';
 import { cn } from '@/utils/cn';
+import { MouseEvent } from 'react';
 
 interface Props {
   channelId: Channel['channelId'];
@@ -11,7 +12,8 @@ interface Props {
 
 const ChannelExitButton = ({ channelId, className }: Props) => {
   const exitChannel = useChatStore((state) => state.exitChannel);
-  const handleChannelExit = () => {
+  const handleChannelExit = (e: MouseEvent) => {
+    e.stopPropagation();
     if (window.confirm('현재 채팅방을 나가시겠습니까?')) {
       const userId = useAuthStore.getState().userInfo.userId;
       exitChannel(userId, channelId);
@@ -20,7 +22,7 @@ const ChannelExitButton = ({ channelId, className }: Props) => {
 
   return (
     <button
-      onClick={handleChannelExit}
+      onClick={(e) => handleChannelExit(e)}
       aria-label='채팅방 나가기'
       className={cn(
         'flex items-center text-darkgray hover:text-[#333] h-[38px] gap-[5px]',
