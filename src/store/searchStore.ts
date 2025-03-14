ff139e1e3dc54278e3be5d45955bed673a434cab
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-03-14 10:45:52 +0900

    [Refactor] 커서값 두 개에서 한 개로 변경

--- src/store/searchStore.ts
+++ src/store/searchStore.ts
@@ -1,15 +1,10 @@
-import { FetchChannelMessagesRequest } from '@/types/message.type';
 import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
 
 export interface SearchState {
-  searchDirection: FetchChannelMessagesRequest['direction'];
-  cursors: {
-    prev: number | null;
-    next: number | null;
-  };
   searchMode: boolean;
   searchKeyword: string;
+  searchDirection: 'backward' | 'forward';
   searchCursor: number | null;
 }
 
@@ -20,14 +15,10 @@ export interface SearchAction {
 type SearchStore = SearchState & SearchAction;
 
 export const initialState: SearchState = {
-  searchDirection: 'backward',
-  cursors: {
-    prev: null,
-    next: null,
-  },
-  searchCursor: null,
   searchMode: false,
   searchKeyword: '',
+  searchDirection: 'backward',
+  searchCursor: null,
 };
 
 export const useSearchStore = create<SearchStore>()(
