4e43eff950860cdc55d46abd60dee5a99002850f
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-03-19 15:17:57 +0900

    [Fix] div -> form

--- src/components/molecules/ChatInput.tsx
+++ src/components/molecules/ChatInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Icon from '@/components/atoms/Icon';
 import Avatar from '@/components/atoms/Avatar';
 import useAuthStore from '@/store/authStore';
@@ -14,20 +14,14 @@ const ChatInput = ({ onSubmit, userImage, isPending }: CommentInputProps) => {
   const [comment, setComment] = useState<string>('');
   const { isLoggedIn } = useAuthStore(useShallow((state) => state));
 
-  const handleSubmit = () => {
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (!comment.trim()) {
       alert('댓글 내용을 입력해주세요.');
       return;
     }
     onSubmit(comment);
     setComment('');
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      handleSubmit();
-    }
   };
 
   return (
@@ -38,22 +32,24 @@ const ChatInput = ({ onSubmit, userImage, isPending }: CommentInputProps) => {
           alt='User Avatar'
           className='w-[40px] h-[40px] rounded-full'
         />
-        <div className='w-full bg-lightgray px-3 py-2 rounded-xl flex items-center justify-between'>
+        <form
+          className='w-full bg-lightgray px-3 py-2 rounded-xl flex items-center justify-between'
+          onSubmit={submitHandler}
+        >
           <input
             className='w-full bg-lightgray focus:outline-none'
             placeholder='내용을 입력해주세요.'
             value={comment}
             onChange={(e) => setComment(e.target.value)}
             disabled={isPending || !isLoggedIn}
-            onKeyDown={handleKeyDown}
           />
-          <div
+          <button
             className='bg-white w-[30px] h-[30px] flex items-center justify-center rounded-full cursor-pointer border'
-            onClick={handleSubmit}
+            type='submit'
           >
             <Icon type='arrowLongUp' className='w-[20px] h-[20px]' />
-          </div>
-        </div>
+          </button>
+        </form>
       </div>
     </>
   );
