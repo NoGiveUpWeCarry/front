b18ef8b9607afd6c9c149a5ebcb4f96f65362e02
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-03-25 12:01:12 +0900

    [Refactor] 탭 합성컴포넌트화
    - 마이 페이지에서 사용하는 탭과 검색에서 사용하는 탭을 공용 컴포넌트로 사용하기 쉽도록 합성컴포넌트화 함

--- src/components/organisms/Tabs.tsx
+++ src/components/organisms/Tabs.tsx
@@ -1,14 +1,106 @@
-import TabItem from '@/components/molecules/TabItem';
-import { PropsWithChildren } from 'react';
+import TabButton from '@/components/molecules/TabButton';
+import {
+  createContext,
+  Fragment,
+  PropsWithChildren,
+  useContext,
+  useMemo,
+  useState,
+} from 'react';
 
-const Tabs = ({ children }: PropsWithChildren) => {
+interface TabContextType {
+  active: number;
+  setActive: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const TabContext = createContext<TabContextType | null>(null);
+
+export const useTabContext = () => {
+  const context = useContext(TabContext);
+  if (!context) {
+    throw new Error('useTabContext must be used within a TabContext');
+  }
+  return context;
+};
+
+const TabsMain = ({ children }: PropsWithChildren) => {
+  const [active, setActive] = useState(0);
+
+  const value = useMemo(() => ({ active, setActive }), [active, setActive]);
+
+  return <TabContext.Provider value={value}>{children}</TabContext.Provider>;
+};
+
+export interface TabTriggerProps {
+  index: number;
+}
+
+const TabTrigger = ({
+  children,
+  index,
+  ...rest
+}: PropsWithChildren<TabTriggerProps>) => {
+  const { active, setActive } = useTabContext();
   return (
-    <div className='w-full h-[38px] px-[5px] flex items-center bg-[#d1d1d1] rounded-[5px]'>
+    <TabButton
+      isActive={active === index}
+      onClick={() => setActive(index)}
+      {...rest}
+    >
       {children}
+    </TabButton>
+  );
+};
+
+interface TabPannelProps {
+  index: number;
+}
+
+const TabPannel = ({ children, index }: PropsWithChildren<TabPannelProps>) => {
+  const { active } = useTabContext();
+  return active === index ? children : null;
+};
+
+interface TabTriggersProps {
+  labels: string[];
+  className?: string;
+  divider?: React.ReactNode;
+}
+
+const TabTriggers = ({ labels, className, divider }: TabTriggersProps) => {
+  return (
+    <div className={className}>
+      {labels.map((label, index, arr) => (
+        <Fragment key={index}>
+          <TabTrigger index={index}>{label}</TabTrigger>
+          {index !== arr.length - 1 && divider}
+        </Fragment>
+      ))}
     </div>
   );
 };
 
-Tabs.TabItem = TabItem;
+interface TabPannelsProps {
+  components: React.ReactNode[];
+}
+
+const TabPannels = ({ components }: PropsWithChildren<TabPannelsProps>) => {
+  return (
+    <>
+      {components.map((component, index) => (
+        <TabPannel key={index} index={index}>
+          {component}
+        </TabPannel>
+      ))}
+    </>
+  );
+};
+
+const Tabs = Object.assign(TabsMain, {
+  Trigger: TabTrigger,
+  Triggers: TabTriggers,
+  Pannel: TabPannel,
+  Pannels: TabPannels,
+});
 
 export default Tabs;
