38be03a5910ce97587c5b8c024e37e54909cbad0
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-04-10 19:25:13 +0900

    [Refactor] 파일 업로드 로직 가독성 개선

--- src/components/molecules/FileUploadButton.tsx
+++ src/components/molecules/FileUploadButton.tsx
@@ -19,24 +19,29 @@ const FileUploadButton = ({ accept, children, className, onChange }: Props) => {
   const inputId = useId();
   const { setFile } = useFileContext();
 
+  const handleImageFile = async (file: File) => {
+    const optimizedImage = await optimizeImage(file);
+    if (optimizedImage.size > LIMIT.MAX_IMAGE_SIZE) {
+      alert(
+        `이미지 크기가 너무 큽니다. 최대 ${LIMIT.MAX_IMAGE_SIZE / 1e6}MB 까지 업로드 가능합니다.`
+      );
+      setFile(null);
+      return null;
+    }
+    return optimizedImage;
+  };
+
   const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
-    if (file) {
-      if (file.type.match(/image\/*/)) {
-        const optimizedImage = await optimizeImage(file);
-        if (optimizedImage.size > LIMIT.MAX_IMAGE_SIZE) {
-          alert(
-            `이미지 크기가 너무 큽니다. 최대 ${LIMIT.MAX_IMAGE_SIZE / 1e6}MB 까지 업로드 가능합니다.`
-          );
-          setFile(null);
-          e.target.value = '';
-          return;
-        }
-        setFile(optimizedImage);
-      } else {
-        setFile(file);
-      }
+    let optimizedImage: File | null = null;
+    if (!file) return;
+
+    if (file.type.match(/image\/*/)) {
+      optimizedImage = await handleImageFile(file);
     }
+
+    setFile(optimizedImage || file);
+
     if (onChange) onChange(e);
   };
 
