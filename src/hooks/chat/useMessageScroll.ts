0ec6a556bbcc8d749e2ab3e443b45beb370739b8
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-07-05 20:05:24 +0900

    [Fix] 채팅 보냈을 때 스크롤 아래로 내려가지 않는 버그 수정

--- src/hooks/chat/useMessageScroll.ts
+++ src/hooks/chat/useMessageScroll.ts
@@ -31,28 +31,32 @@ export const useMessageScroll = ({
   }, [isTopInView]);
 
   // 스크롤 위치 조정
+  // invalidate query를 실행하면 새로운 메시지가 추가되어도 메시지의 개수는 변하지 않아서 스크롤 위치가 변하지 않음
+  // fetchNextPage 또는 fetchPreviousPage 시에는 스크롤 위치를 계산해야되고, 자기가 보냈을 때는 무조건 맨 아래로 이동해야됨
   useEffect(() => {
     const scrollContainer = scrollContainerRef.current;
     if (!scrollContainer) return;
 
-    setTimeout(() => {
-      const newHeight = scrollContainer.scrollHeight;
-      const heightDiff = newHeight - previousHeightRef.current;
-      // searchMode일 경우 MessageBubble 컴포넌트의 scrollIntoView 에 의해 스크롤 위치가 조정됨
-      if (!searchMode && heightDiff > 0) {
-        switch (direction) {
-          case 'backward': {
-            scrollContainer.scrollTop += heightDiff;
-            break;
-          }
-          case 'forward': {
-            scrollContainer.scrollTop -= heightDiff;
-            break;
-          }
+    const newHeight = scrollContainer.scrollHeight;
+    const heightDiff = newHeight - previousHeightRef.current;
+    // searchMode일 경우 MessageBubble 컴포넌트의 scrollIntoView 에 의해 스크롤 위치가 조정됨
+    if (!searchMode && heightDiff === 0) {
+      scrollContainer.scrollTop = scrollContainer.scrollHeight;
+      return;
+    }
+    if (!searchMode && heightDiff > 0) {
+      switch (direction) {
+        case 'backward': {
+          scrollContainer.scrollTop += heightDiff;
+          break;
+        }
+        case 'forward': {
+          scrollContainer.scrollTop -= heightDiff;
+          break;
         }
       }
-      previousHeightRef.current = newHeight;
-    });
+    }
+    previousHeightRef.current = newHeight;
   }, [messages, direction, searchMode]);
 
   return {
